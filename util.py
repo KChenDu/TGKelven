85b713177f96e26ad3f42456347f7a5d2164e0f3
KChenDu <kelvenchendu@gmail.com>
2022-09-09 21:24:34 -0300

    add files

---
+++ util.py
@@ -0,0 +1,56 @@
+import numpy as np
+import pandas as pd
+import tensorflow as tf
+import matplotlib.pyplot as plt
+
+
+def get_test_curve(length=3653, mean=0, amplitude=10, period=365.2524, noise=0.05):
+    x = np.arange(length)
+    y = mean + amplitude * np.sin(2 * np.pi / period * x)
+    y = np.random.normal(y, noise * y)
+    df = pd.DataFrame(data={'y': y},
+                      index=pd.date_range(end=pd.Timestamp.today(), periods=length))
+    return df
+
+
+def fft_analysis(df, label='y', n_per_day=1):
+    fft = tf.signal.rfft(df[label])
+    f_per_dataset = np.arange(0, len(fft))
+    n_samples = len(df[label])
+    years_per_dataset = n_samples / (n_per_day * 365.2524)
+    f_per_year = f_per_dataset / years_per_dataset
+    plt.step(f_per_year, np.abs(fft))
+    plt.xscale('log')
+    plt.ylim(0, 400000)
+    plt.xlim([0.1, max(plt.xlim())])
+    plt.xticks([1, 365.2524], labels=['1/Year', '1/day'])
+    plt.xlabel('Frequency (log scale)')
+    plt.savefig('images/fft_' + label + '.eps')
+    plt.show()
+
+
+def split(df, input_steps, output_steps):
+    val_size = int(len(df) / 10)
+    return df[:-output_steps - val_size + input_steps], \
+           df[-output_steps - val_size: -output_steps],\
+           df[-input_steps - output_steps:]
+
+
+def normalize(train_df, val_df, test_df):
+    train_mean = train_df.mean()
+    train_std = train_df.std()
+    return (train_df - train_mean) / train_std,\
+           (val_df - train_mean) / train_std,\
+           (test_df - train_mean) / train_std
+
+
+def make_dataset(data, input_steps, output_steps, label, batch_size):
+    inputs = data[:-output_steps]
+    targets = np.lib.stride_tricks.sliding_window_view(data[[label]][input_steps:],
+                                                       output_steps,
+                                                       axis=0).transpose((0, 2, 1))
+    return tf.keras.utils.timeseries_dataset_from_array(inputs,
+                                                        np.array(targets),
+                                                        input_steps,
+                                                        batch_size=batch_size,
+                                                        shuffle=True)
