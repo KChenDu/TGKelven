ef7af82c1c53a3d118554abbd3e80672d6ced246
Orange <kelvenchendu@gmail.com>
2022-10-09 17:31:40 -0300

    improve matlab implementation

--- main.m
+++ main.m
@@ -2,31 +2,38 @@ clear
 close all
 clc
 
+% read data
 mktdata = readtable("datas/ERJ_PBR_VALE_weekly.csv");
 
+% setup variables
 label = "ERJ";
 inputSteps = 104;
 outputSteps = 52;
 
+% split train/test data
 n = height(mktdata) - outputSteps;
 trainData = mktdata(1 : n, 2 : 4);
 testData = mktdata(n - inputSteps + 1 : end, 2 : 4);
 clear mktdata
 
-net = narxnet(1 : inputSteps, 1 : inputSteps, 43);
+% build neural network
+net = narxnet(1 : inputSteps, 1 : inputSteps, 32);
 net.numInputs = 3;
 net.inputConnect = [1 1 1; 0 0 0];
 net.inputs{1}.name = "x1";
 net.inputs{3}.name = "x2";
 net.inputs{3}.processFcns = {'removeconstantrows', 'mapminmax'};
 net.inputWeights{1, 3}.delays= 1 : inputSteps;
-net.divideParam.trainRatio = 0.5;
-net.divideParam.valRatio = 0.5;
+net.divideParam.trainRatio = 0.8;
+net.divideParam.valRatio = 0.2;
 net.divideParam.testRatio = 0;
 net.trainParam.min_grad = 0;
 
-X = table2cell(trainData(:, trainData.Properties.VariableNames ~= label)).';
-T = table2cell(trainData(:, label)).';
+% prepare inputs and train
+X = table2cell(normalize(trainData(:, trainData.Properties.VariableNames ~= label))).';
+T = table2array(trainData(:, label));
+first = T(end);
+T = num2cell(normalize(T)).';
 [x, xi, ai, t] = preparets(net, X, {}, T);
 net = train(net, x, t, xi, ai);
 
@@ -35,9 +42,14 @@ net = train(net, x, t, xi, ai);
 net = closeloop(net);
 % view(net)
 
-X = table2cell(testData(:, testData.Properties.VariableNames ~= label)).';
-T = table2cell(testData(:, label)).';
+X = table2cell(normalize(testData(:, testData.Properties.VariableNames ~= label))).';
+T = table2array(testData(:, label));
+train_std = std(T);
+T = num2cell(normalize(T)).';
 [x, xi, ai, t] = preparets(net, X, {}, T);
-y = net(x ,xi, ai);
+y = cell2mat(net(x ,xi, ai));
 
-writecell(y, 'results/' + label + '_NARX_prediction.csv');
+y = y * train_std;
+y = y + first - y(1);
+
+writematrix(y, 'results/' + label + '_NARX_prediction.csv');
